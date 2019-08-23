6771c29f66ce4e00011f74c0ed97453852b479f7
BigCodilo <vladgirko1@gmail.com>
2019-08-23 16:57:22 +0300

    updates here

--- loger/print.go
+++ loger/print.go
@@ -58,39 +58,3 @@ func (level *LogLevel) println(data interface{}) error{
 	}
 	return nil
 }
-
-//--------WITH_CACHE----------//
-
-//Добавляет запись в кеш если уровень логирования включен
-func (level *LogLevel) PrintWithCache(data...interface{}) error{
-	if level.TurnedOn{
-		level.Cache.SaveToCache(data, level.LevelName)
-	} else{
-		return errors.New(level.LevelName + " level is off now")
-	}
-	return nil
-}
-
-//Раз в какой-то промежуток времени выводит все записи с кеша и очищает его
-func (level *LogLevel)UnloadCache(){
-	for level.Cache.TurnedOn{
-		logsArray := []LogStruct{}
-		for _, v := range level.Cache.Logs{
-			logsArray = append(logsArray, v)
-		}
-		logeArrayJSON, _ := json.Marshal(logsArray)
-		level.Stream.Write(logeArrayJSON)
-		level.Cache.Logs = []LogStruct{}
-		time.Sleep(level.Cache.SleepTime)
-	}
-}
-
-//Сохраняет в кеш строку которую формирует тут
-func (cache *LogCache) SaveToCache(data interface{}, levelName string) error{
-	logStruct := NewLogStruct()
-	logStruct.Level = levelName
-	logStruct.Data = data
-	logStruct.Time = time.Now().Format(time.RFC3339)
-	cache.Logs = append(cache.Logs, logStruct)
-	return nil
-}
